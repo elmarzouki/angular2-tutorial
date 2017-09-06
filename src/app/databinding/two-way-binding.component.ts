44845ebf5b021ac056eb84904fa87a6e1e752622
iSuperMostafa <mostafam.elmarzouki@gmail.com>
2017-09-06 10:16:28 +0200

    Two-Way Binding

--- src/app/databinding/two-way-binding.component.ts
+++ src/app/databinding/two-way-binding.component.ts
@@ -3,9 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-two-way-binding',
   template: `
-    <label>username: </label>
     <input type="text" [(ngModel)]="user.name">
-    <input type="text" [(ngModel)]="user.name">
+    <p>username: {{user.name}}</p>
   `,
   styles: []
 })
