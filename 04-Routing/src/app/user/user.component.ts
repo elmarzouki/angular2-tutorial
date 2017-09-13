6e09f3a60c6c53559cce46c5223671f7043249d3
iSuperMostafa <mostafam.elmarzouki@gmail.com>
2017-09-13 13:51:47 +0200

    Understanding Navigation Paths

--- 04-Routing/src/app/user/user.component.ts
+++ 04-Routing/src/app/user/user.component.ts
@@ -4,6 +4,10 @@ import { Component } from '@angular/core';
   selector: 'app-user-component',
   template: `
       <h1>User Component</h1>
+      <!-- nested routes -->
+      <!-- <a [routerLink]="['user']">User</a> -->
+      <a [routerLink]="['/user']">User</a>
+      <a [routerLink]="['../']">Back to Home</a>
     `
 })
 export class UserComponent {
