6d03d1bcdb8c56ba89fce1c6dfafcf4104d7888f
iSuperMostafa <mostafam.elmarzouki@gmail.com>
2017-09-25 14:15:00 +0200

    Input Validation

--- 05-Forms/src/app/data-driven/data-driven.component.ts
+++ 05-Forms/src/app/data-driven/data-driven.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
     selector: 'data-driven',
     templateUrl: 'data-driven.component.html'
 })
 export class DataDrivenComponent {
     dataDrivenForm = new FormGroup({
-		'username': new FormControl(),
-		'email': new FormControl(),
-		'password': new FormControl()
+		'username': new FormControl('iSuperMostafa', Validators.required),
+		'email': new FormControl('', [
+			Validators.required, 
+			Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]),
+		'password': new FormControl('', Validators.required)
 	});
 
     onSubmit() {
