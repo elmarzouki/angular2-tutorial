17574789fde8f3cb987b7e72dd87425904bb488e
iSuperMostafa <mostafam.elmarzouki@gmail.com>
2017-09-25 11:30:12 +0200

    Creating a Form Data-Driven

---
+++ 05-Forms/src/app/data-driven/data-driven.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+@Component({
+    selector: 'data-driven',
+    templateUrl: 'data-driven.component.html'
+})
+export class DataDrivenComponent {
+    dataDrivenForm: FormGroup;
+
+    constrctor() {
+    	this.dataDrivenForm = new FormGroup({
+    		'username': new FormControl(),
+    		'email': new FormControl(),
+    		'password': new FormControl()
+    	});
+    }
+}
