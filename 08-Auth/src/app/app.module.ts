927f5dd6139a944df635cab78783b5102839dc28
iSuperMostafa <mostafam.elmarzouki@gmail.com>
2017-09-27 15:07:42 +0200

    setup

--- 08-Auth/src/app/app.module.ts
+++ 08-Auth/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent }   from './app.component';
+import { HeaderComponent } from "./shared/header.component";
+import { SigninComponent } from "./unprotected/signin.component";
+import { SignupComponent } from "./unprotected/signup.component";
+import { ProtectedComponent } from "./protected/protected.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SigninComponent,
+    SignupComponent,
+    ProtectedComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
