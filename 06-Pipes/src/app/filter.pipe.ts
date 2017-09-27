cb4eb9f7899e3ccae553298c14d7265defcb5253
iSuperMostafa <mostafam.elmarzouki@gmail.com>
2017-09-27 10:28:57 +0200

    Impure Pipes

--- 06-Pipes/src/app/filter.pipe.ts
+++ 06-Pipes/src/app/filter.pipe.ts
@@ -1,7 +1,8 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'filter'
+  name: 'filter',
+  pure: false	// impure pipe or async pipe
 })
 export class FilterPipe implements PipeTransform {
 
