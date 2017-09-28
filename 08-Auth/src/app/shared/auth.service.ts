b7f2af72fead61151892002823cb9f6e70e47df7
Mostafa El-Marzouki <mostafam.elmarzouki@gmail.com>
2017-09-28 16:36:47 +0200

    Finishing Touches

--- 08-Auth/src/app/shared/auth.service.ts
+++ 08-Auth/src/app/shared/auth.service.ts
@@ -1,8 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { User } from './user.interface';
 
 declare var firebase: any;
 
+@Injectable()
 export class AuthService {
+	constructor(private router: Router) {}
+
 	signupUser(user: User) {
 		firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
 		.catch(function(error) {
@@ -37,5 +43,6 @@ export class AuthService {
 
 	logout() {
 		firebase.auth().signOut();
+		this.router.navigate(['/signin']);
 	}
 }
